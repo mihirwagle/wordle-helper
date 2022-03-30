046fa4865836b5c1e5718c135ac99e53d56ca2f6
Mihir Wagle <mihir.wagle@nuance.com>
2022-03-30 16:52:30 -0400

    Exit on bad input.

--- play.py
+++ play.py
@@ -14,24 +14,28 @@ def build_results(color):
 def play_game():
     # Load the word list
     words = [word.replace("\r", "").replace("\n", "").upper() for word in open("word-list.txt").readlines()]
-    print("Starting game now.")
+    print("Starting game helper now.")
     print("Please enter your input as follows:")
     print("SNORT G_G_Y")
     print("Here G == Green, Y == Yellow, _==Miss")
     for round in range(5):
         print("Round ",(round+1))
         input_str = input("Enter input: ").upper()
+        if len(input_str) != 11 or " " not in input:
+            print("Invalid entry, exiting helper.")
+            return
         input_split = input_str.split()
         last_guess = input_split[0]
         color = input_split[1]
         if len(last_guess) != 5 or len(color) != 5:
-            print("Invalid entry, exiting program.")
+            print("Invalid entry, exiting helper.")
             return
         # Build results enum using colors
         results = build_results(color)
         words = filter_words(last_guess, results, words)
         print("Possible words are: ")
         print(words)
+    print("Game over! Exiting helper")
     return
 
 play_game()
